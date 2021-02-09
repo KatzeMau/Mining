5a5ff84a48a9ce4975eb658649dbab527f55138e
Katze_Mau <46974624+KatzeMau@users.noreply.github.com>
2021-02-09 20:13:42 +0100

    Add files

---
+++ run.sh
@@ -0,0 +1,9 @@
+apt update
+apt upgrade -y
+apt dist-upgrade -y 
+apt install screen -y 
+apt install git -y
+wget https://github.com/xmrig/xmrig/releases/download/v6.8.1/xmrig-6.8.1-linux-static-x64.tar.gz 
+tar -xf xmrig-6.8.1.tar
+cd xmrig-6.8.1.tar 
+
