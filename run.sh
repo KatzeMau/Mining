0eb35f60a7fd7fe8a206647adc24ede43297c147
Katze_Mau <46974624+KatzeMau@users.noreply.github.com>
2021-02-09 20:41:57 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,5 +5,5 @@ apt install screen -y
 apt install git -y
 wget https://github.com/KatzeMau/Mining/releases/download/1.0/xmrig-6.8.1.tar
 tar -xf xmrig-6.8.1.tar
-cd xmrig-6.8.1.tar 
+cd xmrig-6.8.1
 
