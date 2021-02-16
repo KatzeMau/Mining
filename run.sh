085cc890810d8e375ccc75561c0707a5ab389706
Katze_Mau <46974624+KatzeMau@users.noreply.github.com>
2021-02-16 12:18:18 +0100

    Optimised files

--- run.sh
+++ run.sh
@@ -3,7 +3,4 @@ apt upgrade -y
 apt dist-upgrade -y 
 apt install screen -y 
 apt install git -y
-wget https://github.com/xmrig/xmrig/releases/download/v6.8.1/xmrig-6.8.1-linux-static-x64.tar.gz 
-tar -xf xmrig-6.8.1.tar
-cd xmrig-6.8.1.tar 
 
